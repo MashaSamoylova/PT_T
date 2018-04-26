a94d0262552e247fbe1b7709c75d7578c6e27236
MashaSamoylova <mashanedyak@gmail.com>
2018-04-26 19:07:36 +0700

    [+]main

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+#!/usr/bin/env python3.5
 import os
+import importlib
 
-def check_ExecutablePython(file_name):
-    return file_name.split(".")[-1:][0]=="py" and os.access(file_name, os.X_OK) 
+def check_executable_python(file_name):
+    return file_name.split(".")[-1:][0]=="py"
 
 def main():
-    files = os.listdir("./scripts")
-
-    for f in filter(check_ExecutablePython, os.listdir(./scripts)):
-        import f
-        f.main()
+    for f in filter(check_executable_python, os.listdir("./scripts")):
+        mod = importlib.import_module("scripts."+f[:-3])
+        mod.main()
 
 if __name__=="__main__":
     main()
---
+++ scripts/000_test.py
@@ -0,0 +1,5 @@
+##!/usr/bin/env python3.5
+print("module imported")
+
+def main():
+    print("ALL IS OK")
