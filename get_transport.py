d74bd8ba6c4edaed955c979cc9d5cb8f56196721
MashaSamoylova <mashanedyak@gmail.com>
2018-05-10 18:50:38 +0700

    [*]pep8 get_transport.py

--- get_transport.py
+++ get_transport.py
@@ -21,11 +21,16 @@ class MySQLError(TransportError):
     pass
 
 class SSH():
-    def __init__(self, host, port_, login, passwd):
+    def __init__(self, host, port, login, passwd):
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         try:
-            self.client.connect(hostname=host, username=login, password=passwd, port=port_)
+            self.client.connect(
+                    hostname=host, 
+                    username=login, 
+                    password=passwd, 
+                    port=port
+            )
         except 	paramiko.BadHostKeyException:
             raise TransportConnectionError("BadHostKeyException")
         except 	paramiko.AuthenticationException:
@@ -91,9 +96,13 @@ def get_transport(transport_name, host="", port="",login="", password=""):
     if transport_name not in transport_names:
         raise UnknownTransport
 
-    if not host: host = config['host']
-    if not port: port = config['transports'][transport_name]['port']
-    if not login: login = config['transports'][transport_name]['login']
-    if not password: password = config['transports'][transport_name]['password']
+    if not host: 
+        host = config['host']
+    if not port: 
+        port = config['transports'][transport_name]['port']
+    if not login: 
+        login = config['transports'][transport_name]['login']
+    if not password: 
+        password = config['transports'][transport_name]['password']
 
     return transport_names[transport_name](host,port,login, password)
